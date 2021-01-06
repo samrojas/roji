fc04c823b0462705f07ae1917b73ff2f8ead5a38
michaela <michaela.bateman001@gmail.com>
2021-01-06 17:07:12 -0600

    created react app

---
+++ sammys-website/src/components/Header.js
@@ -0,0 +1,13 @@
+function Header(){
+    return(
+        <header className="header">
+            <image src="/" alt="logo"/>
+            <div className='button-container'>
+                <button>Work</button>
+                <button>About</button>
+            </div>
+        </header>
+    )
+}
+
+export default Header
