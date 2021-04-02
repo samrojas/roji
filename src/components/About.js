eb6331d5e331f1c7e0bd74b90c2fb9a9763556c3
michaela <michaela.bateman001@gmail.com>
2021-04-01 19:25:56 -0500

    update file structure

---
+++ src/components/About.js
@@ -0,0 +1,29 @@
+function About(){    
+    return(
+                
+    <div className="about">
+       <div className='about-pic'>
+
+       </div>
+       <div className='paragraph-container'>
+            <div className='header'>
+                <p>paragraph 1</p>
+            </div>
+            <div className='paragraph'>
+                
+            </div>
+       </div>
+       <div className='paragraph-container'>
+            <div className='header'>
+                <p>paragraph 2</p>
+            </div>
+            <div className='paragraph'>
+
+            </div>
+       </div>
+    </div>
+  
+    )
+}
+
+export default About
