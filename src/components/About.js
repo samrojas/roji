118172bfbfbeb37639f09df1996c08a83e2ed8e3
Sam Rojas <41696912+samrojas@users.noreply.github.com>
2021-04-16 16:18:42 -0400

    more photos added

--- src/components/About.js
+++ src/components/About.js
@@ -15,10 +15,17 @@ function About(){
        </div>
        <div className='paragraph-container'>
             <div className='header'>
-                <p>paragraph 2</p>
+                <p></p>
             </div>
             <div className='paragraph'>
-
+                <p>Throughout time, people have used the sum of their blended experiences to forge something new through whatever medium they choose. Driven by a fear of the unknown future, many create to define that unknown for themselves with their own works.
+                <br></br>In that same spirit, this website exists as the home for creations crafted by Sam Rojas. 
+                <br></br>He resides mostly in Boston, MA, but lives and works from anywhere.</p>
+                <hr></hr>
+                <p>Follow - 
+                <br></br>
+                Links - 
+                </p>
             </div>
        </div>
     </div>
