93a779fe63fc1cc796cb586cc95bc3d448816638
Sam Rojas <41696912+samrojas@users.noreply.github.com>
2021-04-21 02:08:09 -0400

    style updates and about page update

--- src/components/About.js
+++ src/components/About.js
@@ -10,9 +10,43 @@ function About(){
                 <p></p>
             </div>
             <div className='paragraph'>
-                <p>Throughout time, people have used the sum of their blended experiences to forge something new through whatever medium they choose. 
-                <br></br><br></br>Driven by a fear of the unknown future, many <i>create</i> to define that unknown for themselves with their own works.
-                <br></br><br></br>In that same spirit, this website exists as the home for creations crafted by Sam Rojas. 
+                <p>
+                    Throughout time, people have used the sum of their blended experiences to forge something new through whatever medium they choose. 
+                    <br></br><br></br>
+                    Driven by a fear of the unknown future, many <i>create</i> to define that unknown for themselves with their own works.
+                    <br></br><br></br>
+                    In that same spirit, this website exists as the home for creations crafted by <b>Sam Rojas</b>. 
+                </p>
+
+                <br></br>
+                <hr></hr>
+                <br></br>
+                <p><b>Connect via</b>
+                    <br></br>
+                    <a href='mailto:samrojas95@gmail.com'>Email</a>
+                    <br></br>
+                    <a href='https://open.spotify.com/user/12147008411?si=6f9e36e5eee04e7d'>Spotify</a>
+                    <br></br>
+                    <a href='https://letterboxd.com/rojiroji/'>Letterboxd</a>
+                </p>
+
+                <br></br>
+
+                <p><b>Webring <i>(no website should be a dead-end)</i></b>
+                    <br></br>
+                    <a href='https://theuselessweb.com/'>The Useless Web</a>
+                    <br></br>
+                    <a href='https://808303.studio/'>Roland 808303 Studio</a>
+                    <br></br>
+                    <a href='https://everynoise.com/engenremap.html'>Every Noise at Once</a>
+                    <br></br>
+                    <a href='https://orb.farm/'>Orb.Farm</a>
+                    <br></br>
+                    <a href='https://www.strandbeest.com/'>Strandbeest</a>
+                    <br></br>
+                    <a href='http://hiroshinagai.com/'>Art by Hiroshi Nagai</a>
+                    <br></br>
+                    <a href='https://poolside.fm/'>Poolside.fm</a>
                 </p>
                 <br></br>
                 <hr></hr>
